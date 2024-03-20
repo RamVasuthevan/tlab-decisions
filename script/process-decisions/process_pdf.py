c6b985d97cdea91526d4da32bc5ee4cb3b26c1b5
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2024-03-20 17:21:32 +0000

    Checkpoint

--- script/process-decisions/process_pdf.py
+++ script/process-decisions/process_pdf.py
@@ -10,8 +10,18 @@ def extract():
     print(f"Total pages: {len(reader.pages)}")
     return reader
 
-def transform(reader):
-    pass
+def transform_bookmarks(reader):
+    bookmarks_details = []
+    extract_bookmarks(reader.outline, bookmarks_details)
+    
+    decisions = []
+    for i, (title, _) in enumerate(bookmarks_details):
+        decisions.append({
+            'bookmark_name': str(title),
+            'year': "2017",
+            'file_name': f"{i:02}_{''.join(x for x in title if x.isalnum() or x in ' ._-')}.pdf"
+        })
+    return decisions
 
 def extract_bookmarks(outlines, details, parent_title=""):
     for bookmark in outlines:
@@ -23,9 +33,8 @@ def extract_bookmarks(outlines, details, parent_title=""):
             page_num = reader.get_destination_page_number(bookmark) + 1
             details.append((full_title, page_num))
 
-def load_splited_pdfs(reader):
+def load_splited_pdfs(reader, decisions):
     OUTPUT_DIR = "../../website/assets/tlab-decisions/2017"
-
     if os.path.exists(OUTPUT_DIR):
         shutil.rmtree(OUTPUT_DIR)
     os.makedirs(OUTPUT_DIR)
@@ -33,31 +42,14 @@ def load_splited_pdfs(reader):
     bookmarks_details = []
     extract_bookmarks(reader.outline, bookmarks_details)
     
-    decisions = []
-    
     for i, (title, start_page) in enumerate(bookmarks_details):
         writer = PyPDF2.PdfWriter()
         end_page = bookmarks_details[i + 1][1] - 1 if i + 1 < len(bookmarks_details) else len(reader.pages)
-        
         for page_num in range(start_page - 1, end_page):
             writer.add_page(reader.pages[page_num])
-        
-        # Sanitizing title for filename usage
-        sanitized_title = "".join(x for x in title if x.isalnum() or x in " ._-")
-        # Creating filename with leading zeros for the index and sanitized title
-        filename = f"{i:02}_{sanitized_title}.pdf"
-        filepath = os.path.join(OUTPUT_DIR, filename)
-        
+        filepath = os.path.join(OUTPUT_DIR, decisions[i]['file_name'])
         with open(filepath, 'wb') as outfile:
             writer.write(outfile)
-        
-        decisions.append({
-            'bookmark_name': str(title),  # Explicitly converting to string
-            'file_name': filename,  # Use the constructed filename
-            'year': "2017"  # Static year for now
-        })
-
-    return decisions
 
 def load_data_file(decisions):
     OUTPUT_FILE = "../../website/_data/decisions.yml"
@@ -66,5 +58,6 @@ def load_data_file(decisions):
 
 if __name__ == "__main__":
     reader = extract()
-    decisions = load_splited_pdfs(reader)
+    decisions = transform_bookmarks(reader)
+    load_splited_pdfs(reader, decisions)
     load_data_file(decisions)
